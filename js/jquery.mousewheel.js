cbfff15f9f26a7280b9c90d4dd5082cd5475d1ff
Sam Wu <sjwu@ualberta.ca>
2017-06-11 17:19:46 -0700

    customize presentation colors

--- js/jquery.mousewheel.js
+++ js/jquery.mousewheel.js
@@ -1,4 +1,5 @@
-/*! Copyright (c) 2013 Brandon Aaron (http://brandon.aaron.sh)
+/*! Copyright (c) 2013 Brandon Aaron
+(http://brandon.aaron.sh)
  * Licensed under the MIT License.
  *
  * Version: 3.1.4
