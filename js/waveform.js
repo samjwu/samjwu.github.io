a823e4c7bf049c135ee144a3ea2866a6a9771be7
samjwu <sjwu@ualberta.ca>
2023-04-01 22:26:12 -0600

    equalizer functionality deprecated

--- js/waveform.js
+++ js/waveform.js
@@ -14,102 +14,104 @@ document.addEventListener('DOMContentLoaded', function() {
         backgroundColor: 'black',
         cursorColor: 'orange',
         height: 300,
-        responsive: true
+        responsive: true,
+        barWidth: 5,
+        barGap: 2,
     });
 
-    // Equalizer
-    wavesurfer.on('ready', function() {
-        let EQ = [
-            {
-                f: 32,
-                type: 'lowshelf'
-            },
-            {
-                f: 64,
-                type: 'peaking'
-            },
-            {
-                f: 125,
-                type: 'peaking'
-            },
-            {
-                f: 250,
-                type: 'peaking'
-            },
-            {
-                f: 500,
-                type: 'peaking'
-            },
-            {
-                f: 1000,
-                type: 'peaking'
-            },
-            {
-                f: 2000,
-                type: 'peaking'
-            },
-            {
-                f: 4000,
-                type: 'peaking'
-            },
-            {
-                f: 8000,
-                type: 'peaking'
-            },
-            {
-                f: 16000,
-                type: 'highshelf'
-            }
-        ];
+    // // Equalizer
+    // wavesurfer.on('ready', function() {
+    //     let EQ = [
+    //         {
+    //             f: 32,
+    //             type: 'lowshelf'
+    //         },
+    //         {
+    //             f: 64,
+    //             type: 'peaking'
+    //         },
+    //         {
+    //             f: 125,
+    //             type: 'peaking'
+    //         },
+    //         {
+    //             f: 250,
+    //             type: 'peaking'
+    //         },
+    //         {
+    //             f: 500,
+    //             type: 'peaking'
+    //         },
+    //         {
+    //             f: 1000,
+    //             type: 'peaking'
+    //         },
+    //         {
+    //             f: 2000,
+    //             type: 'peaking'
+    //         },
+    //         {
+    //             f: 4000,
+    //             type: 'peaking'
+    //         },
+    //         {
+    //             f: 8000,
+    //             type: 'peaking'
+    //         },
+    //         {
+    //             f: 16000,
+    //             type: 'highshelf'
+    //         }
+    //     ];
 
-        // Create filters
-        let filters = EQ.map(function(band) {
-            let filter = wavesurfer.backend.ac.createBiquadFilter();
-            filter.type = band.type;
-            filter.gain.value = 0;
-            filter.Q.value = 1;
-            filter.frequency.value = band.f;
-            return filter;
-        });
+    //     // Create filters
+    //     let filters = EQ.map(function(band) {
+    //         let filter = wavesurfer.backend.ac.createBiquadFilter();
+    //         filter.type = band.type;
+    //         filter.gain.value = 0;
+    //         filter.Q.value = 1;
+    //         filter.frequency.value = band.f;
+    //         return filter;
+    //     });
 
-        // Connect filters to wavesurfer
-        wavesurfer.backend.setFilters(filters);
+    //     // Connect filters to wavesurfer
+    //     wavesurfer.backend.setFilters(filters);
 
-        // Delete old range sliders
-        var inputs = document.getElementsByTagName('input');
-        while (inputs.length) inputs[0].parentNode.removeChild(inputs[0]);
+    //     // Delete old range sliders
+    //     var inputs = document.getElementsByTagName('input');
+    //     while (inputs.length) inputs[0].parentNode.removeChild(inputs[0]);
 
-        // Bind filters to vertical range sliders
-        let container = document.querySelector('#equalizer');
-        filters.forEach(function(filter) {
-            let input = document.createElement('input');
-            Object.assign(input, {
-                type: 'range',
-                min: -40,
-                max: 40,
-                value: 0,
-                title: filter.frequency.value,
-            });
-            input.style.display = 'inline-block';
-            input.setAttribute('orient', 'vertical');
-            wavesurfer.util.style(input, {
-                webkitAppearance: 'slider-vertical',
-                width: '50px',
-                height: '300px'
-            });
-            container.appendChild(input);
+    //     // Bind filters to vertical range sliders
+    //     let container = document.querySelector('#equalizer');
+    //     filters.forEach(function(filter) {
+    //         let input = document.createElement('input');
+    //         Object.assign(input, {
+    //             type: 'range',
+    //             min: -40,
+    //             max: 40,
+    //             value: 0,
+    //             title: filter.frequency.value,
+    //         });
+    //         input.style.display = 'inline-block';
+    //         input.setAttribute('orient', 'vertical');
+    //         wavesurfer.util.style(input, {
+    //             webkitAppearance: 'slider-vertical',
+    //             width: '50px',
+    //             height: '300px'
+    //         });
+    //         container.appendChild(input);
 
-            let onChange = function(e) {
-                filter.gain.value = ~~e.target.value;
-            };
+    //         let onChange = function(e) {
+    //             filter.gain.value = ~~e.target.value;
+    //         };
 
-            input.addEventListener('input', onChange);
-            input.addEventListener('change', onChange);
-        });
+    //         input.addEventListener('input', onChange);
+    //         input.addEventListener('change', onChange);
+    //     });
 
-        // For debugging
-        wavesurfer.filters = filters;
-    });
+    //     // For debugging
+    //     wavesurfer.filters = filters;
+    // });
 
     // Log errors
     wavesurfer.on('error', function(msg) {
